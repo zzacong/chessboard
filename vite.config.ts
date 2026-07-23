abd73ae7cb9ff97d0b5a8eccc03f5cd27e17226a
Zac Ong <61817066+zzacong@users.noreply.github.com>
2026-07-23 17:36:34 +1000

    build: integrate React compiler via babel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
   worker: {
     format: "es",
   },
